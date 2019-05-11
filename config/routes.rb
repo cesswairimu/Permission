bb87fa36b998320b5d8ed8a11dece4b0f8a4fdc8
VinneyJ <vincentjayden49@gmail.com>
2019-05-11 17:00:57 +0300

    scaffold user

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :regions
   resources :cities
   resources :countries
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
