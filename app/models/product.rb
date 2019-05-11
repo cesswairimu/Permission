118d52437c57c1a3ce17073402a8328add299fc1
StellaMaris Njage <stellamarisnjage@gmail.com>
2019-05-11 17:31:18 +0300

    implement permission filters (#3)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
   belongs_to :region
+
+  scope :region, -> (region_id) { where region_id: region_id }
+
+  def self.city(id)
+    joins(region: :city).where(cities: {id: id})
+  end
+
+  def self.country(id)
+    joins(region: [city: :country]).where(countries: {id: id})
+  end
 end
